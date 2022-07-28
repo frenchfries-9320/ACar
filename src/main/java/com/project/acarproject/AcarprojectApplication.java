70261918f5495d4d877da5ef09186dfbd3252dfb
frenchfries-9320 <87230106+frenchfries-9320@users.noreply.github.com>
2022-07-28 09:23:32 +0900

    Create Project

---
+++ src/main/java/com/project/acarproject/AcarprojectApplication.java
@@ -0,0 +1,13 @@
+package com.project.acarproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcarprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcarprojectApplication.class, args);
+	}
+
+}
